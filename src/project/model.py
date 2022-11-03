ccc73af0de332f90c70cf754e2151b5073bc7002
tamofplease <riddimdubstep@yahoo.co.jp>
2022-11-03 23:48:18 +0900

    ✨ implement project model and it test codes

--- src/project/model.py
+++ src/project/model.py
@@ -1,6 +1,11 @@
 import uuid
+from os import environ,  path
+from typing import List
+from shutil import rmtree
 from pydantic import BaseModel, Field
+from dotenv import load_dotenv
 import git
+from src.client.csv import CsvClient
 
 
 class NotFound(Exception):
@@ -9,9 +14,57 @@ class NotFound(Exception):
 
 class Project(BaseModel):
     id: str = Field(default_factory=lambda: str(uuid.uuid4()))
+    name: str
     url: str
     commit_hash: str
 
     @classmethod
+    def list(cls) -> List["Project"]:
+        load_dotenv()
+        db_path = environ['DB_ROOT_PATH']
+        client = CsvClient(db_path=db_path)
+        projects = [cls(**record)
+                    for record in client.list_all(table_name='project')]
+        return projects
+
+    def save(self) -> "Project":
+        load_dotenv()
+        db_path = environ['DB_ROOT_PATH']
+        client = CsvClient(db_path=db_path)
+        client.insert(table_name='project', data=(
+            self.id, self.name, self.url, self.commit_hash))
+        return self
+
+    @classmethod
+    def get_by_id(cls, project_id: str) -> "Project":
+        load_dotenv()
+        db_path = environ['DB_ROOT_PATH']
+        client = CsvClient(db_path=db_path)
+        data = client.find_by(table_name='project',
+                              prop='id', value=project_id)
+        if len(data) == 0:
+            raise Exception('data did not found', project_id)
+        return [cls(**record) for record in data][0]
+
+    @classmethod
     def clone(cls, url: str):
-        repo = git.Repo.clone_from(url, "./", no_checkout=True)
+        output_folder = './cloned_projects'
+        name = url.rsplit('/', maxsplit=1)[-1].split('.')[0]
+        output_path = output_folder + '/' + name
+        if path.exists(output_path):
+            rmtree(output_path)
+        repo = git.Repo.clone_from(url, output_path)
+        new_commit = repo.head.commit
+        return Project(
+            name=name,
+            url=url,
+            commit_hash=new_commit.hexsha
+        )
+
+    @classmethod
+    def create_table(cls):
+        load_dotenv()
+        db_path = environ['DB_ROOT_PATH']
+        client = CsvClient(db_path=db_path)
+        client.create_table(table_name='project', columns=[
+                            'id', 'name', 'url', 'commit_hash'])
---
+++ tests/test_project/test_model.py
@@ -0,0 +1,91 @@
+from os import environ, path, makedirs
+from shutil import rmtree
+from typing import List
+from dotenv import load_dotenv
+import pytest
+from src.project.model import Project
+
+
+load_dotenv()
+db_path = environ['DB_ROOT_PATH']
+
+
+@pytest.fixture(name='project_zstd')
+def fixture_project_zstd():
+    return Project(
+        name='zstd',
+        url='https://github.com/facebook/zstd.git',
+        commit_hash='commit_hash_example'
+    )
+
+
+@pytest.fixture(name='project_micropython')
+def fixture_project_micropython():
+    return Project(
+        name='',
+        url='https://github.com/micropython/micropython.git',
+        commit_hash='7c54b6428058a236b8a48c93c255948ece7e718b',
+    )
+
+
+# @pytest.mark.skip(reason="Because it take times.")
+def test_clone(project_zstd):
+    url = project_zstd.url
+    name = project_zstd.name
+    cloned_project = Project.clone(url)
+    output_path = f"./cloned_projects/{name}"
+
+    assert cloned_project.url == url
+    assert cloned_project.name == name
+    assert path.exists(output_path)
+
+    rmtree(output_path)
+
+
+def test_create_table():
+    if not path.exists(db_path):
+        makedirs(db_path)
+    Project.create_table()
+
+    assert path.exists(db_path + '/project.csv')
+
+
+def test_save(project_zstd):
+    project = project_zstd
+    saved_project = project.save()
+
+    assert project.name == saved_project.name
+    assert project.url == saved_project.url
+    assert project.commit_hash == saved_project.commit_hash
+
+
+def test_get_by_id(project_micropython):
+    project = project_micropython
+    saved_project = project.save()
+    fetched_project = Project.get_by_id(project_id=saved_project.id)
+
+    assert saved_project.id == fetched_project.id
+    assert saved_project.name == fetched_project.name
+    assert saved_project.url == fetched_project.url
+    assert saved_project.commit_hash == fetched_project.commit_hash
+
+
+def test_list(project_zstd, project_micropython):
+    fetched_projects: List["Project"] = Project.list()
+
+    assert len(fetched_projects) == 2
+    assert fetched_projects[0].name == project_zstd.name
+    assert fetched_projects[0].url == project_zstd.url
+    assert fetched_projects[0].commit_hash == project_zstd.commit_hash
+    assert fetched_projects[1].name == project_micropython.name
+    assert fetched_projects[1].url == project_micropython.url
+    assert fetched_projects[1].commit_hash == project_micropython.commit_hash
+
+    rmtree(db_path)
+
+
+# @pytest.fixture(scope='function', autouse=True)
+# def scope_function():
+#     print("            setup before function")
+#     yield
+#     print("            teardown after function")
