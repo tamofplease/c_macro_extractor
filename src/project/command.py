6fd9baa6f952b61d1d904d9bacf48f20f7963faa
tamofplease <riddimdubstep@yahoo.co.jp>
2022-11-07 23:12:37 +0900

    ✨ implement add_projects command

--- src/project/command.py
+++ src/project/command.py
@@ -1,6 +1,8 @@
 from os import environ, path
+from shutil import rmtree
 from typing import Optional
 from dotenv import load_dotenv
+import tqdm
 from src.project.model import Project
 
 
@@ -8,12 +10,27 @@ def add_project(url: Optional[str]):
     load_dotenv()
     if not url:
         url = input('\033[96m' + 'Please input github url: ')
-    print('\033[96m' + "clone project...")
     cloned_project = Project.clone(url)
-    db_path, project_root_path = environ['DB_ROOT_PATH'], environ['PROJECT_ROOT_PATH']
+    db_path = environ['DB_ROOT_PATH']
     if not path.exists(db_path):
         Project.create_table()
     cloned_project.save()
-    print('\033[96m' + "complete cloned project to:",
-          project_root_path + '/' + cloned_project.name)
-    print('\033[96m' + "save info to:", db_path)
+
+
+def add_projects(url: Optional[str], force=False):
+    if not url:
+        url = './project_list'
+    db_path, project_root_path = environ['DB_ROOT_PATH'], environ['PROJECT_ROOT_PATH']
+    if path.exists(db_path) or path.exists(project_root_path):
+        if not force:
+            res = input(
+                '\033[93m' + 'Output path are already exist. Are you sure to overwrite? [Y/N]')
+            if res != "Y":
+                return
+        if path.exists(db_path):
+            rmtree(db_path)
+        if path.exists(project_root_path):
+            rmtree(project_root_path)
+    with open(url, mode='r', errors='ignore', encoding='utf-8') as f_object:
+        for github_url in tqdm.tqdm(f_object):
+            add_project(github_url.replace("\n", ''))
--- tests/test_project/test_commands.py
+++ tests/test_project/test_commands.py
@@ -1,17 +1,37 @@
-from os import environ, path
+from os import environ, path, listdir
 from shutil import rmtree
+import pytest
 from dotenv import load_dotenv
-from src.project.command import add_project
+from src.project.command import add_project, add_projects
+
+load_dotenv()
+db_path = environ['DB_ROOT_PATH']
+project_root_path = environ['PROJECT_ROOT_PATH']
 
 
 def test_add_project():
-    load_dotenv()
-    url = "https://github.com/radareorg/radare2.git"
+    url = "https://github.com/octalmage/robotjs.git"
     add_project(url)
-    db_path, project_root_path = environ['DB_ROOT_PATH'], environ['PROJECT_ROOT_PATH']
 
     assert path.exists(db_path + '/project.csv')
-    assert path.exists(project_root_path + '/radare2')
+    assert path.exists(project_root_path + '/robotjs')
 
+
+def test_add_projects():
+    file_path = './test_project_list'
+    add_projects(file_path, True)
+
+    assert path.exists(project_root_path)
+    files = listdir(project_root_path)
+    assert len(files) == 3
+    assert path.exists(project_root_path)
+    file_path = db_path + '/project.csv'
+    with open(file_path, mode='r', errors='ignore', encoding='utf-8') as f_object:
+        assert len(f_object.readlines()) == 4
+
+
+@pytest.fixture(scope='function', autouse=True)
+def scope_function():
+    yield
     rmtree(db_path)
     rmtree(project_root_path)
